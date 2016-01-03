111cc6c1b9e58bc7a00babd5a4af101fa52d9759
f-xyz <andrey.lischina@gmail.com>
2016-01-03 15:32:18 +0200

    .

--- shaders/mandelbrot.fragment.glsl
+++ shaders/mandelbrot.fragment.glsl
@@ -1,24 +1,31 @@
 #define PI 3.14159265
 
 uniform vec2 size;
-uniform vec3 pos;
-uniform vec3 config;
+uniform vec3 pos; // pan / zoom
+uniform vec3 config; // iterations limit etc.
 
 void main() {
 
     float ratio = size.x / size.y;
-    vec2 p = gl_FragCoord.xy / size;
+    vec2 p = gl_FragCoord.xy / size; // [0; 1]
     vec2 center = vec2(3.5*p.x - 2.5, 2.0*p.y - 1.0);
 
+//    gl_FragColor = vec4(p.x, p.y, ratio, 1.);
+//    return;
+
     center /= exp(pos.z);
-    center += vec2(pos.x/*/ratio*/, pos.y);
+    center += vec2(pos.x/ratio, pos.y);
+
+//    center = 1111;
+//    gl_FragColor = vec4(p.x, p.y, 0, 1.);
+//    return;
 
     vec2 point = center;
     float distToCenter = length(p);
 
-    gl_FragColor = vec4(0.0);
+    gl_FragColor = vec4(0.);
 
-    for (float i = 0.0; i >= 0.0; i += 1.0) {
+    for (float i = 0.; i >= 0.; i += 1.) {
         // iterations limit
         if (i > config.x) break;
         // x = x^2 - y^2 + x0
@@ -30,6 +37,7 @@ void main() {
 //            float light = i / config.x * distToCenter;
             float light = i + 1. - log(log(abs(z)) / log(2.));
             light /= 100.0; // max iterations
+//            light *= 10.;
             float b = 1.;
             gl_FragColor = vec4(
                 1.*b*light,
